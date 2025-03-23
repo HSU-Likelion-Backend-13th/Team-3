3742aa1b9b9ca292873855e00e00a9896bc16fcc
gimn70009 <risa1110@hansung.ac.kr>
2025-03-23 16:00:27 +0900

    [섹션7] 5. JPA

--- Nayoung/hello-spring/src/main/java/hello/hello_spring/domain/Member.java
+++ Nayoung/hello-spring/src/main/java/hello/hello_spring/domain/Member.java
@@ -1,8 +1,13 @@
 package hello.hello_spring.domain;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Member {
 
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
     public Long getId() {
@@ -12,9 +17,11 @@ public class Member {
     public void setId(Long id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
