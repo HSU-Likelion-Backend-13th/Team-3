4b083713ee12054c6c163e9da0d7e67a0e6da2e3
woodgeon <ad4086@naver.com>
2025-03-25 13:20:58 +0900

    [섹션 6] 2. 회원 웹 기능 - 등록

--- Geonwoo/hello-spring/src/main/java/com/example/woodgeon/hello_spring/controller/MemberController.java
+++ Geonwoo/hello-spring/src/main/java/com/example/woodgeon/hello_spring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package com.example.woodgeon.hello_spring.controller;
 
+import com.example.woodgeon.hello_spring.domain.Member;
 import com.example.woodgeon.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 // Spring Container 에서 Bean 으로 관리
 @Controller
@@ -25,4 +28,21 @@ public class MemberController {
     // public void setMemberService(MemberService memberService) {
     //     this.memberService = memberService;
     // }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMembersForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member = " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
 }
