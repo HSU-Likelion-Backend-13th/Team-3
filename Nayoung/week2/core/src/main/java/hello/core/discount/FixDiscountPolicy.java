ef09c77a864e9bcddfe317b1cf411425ae9143fb
gimn70009 <risa1110@hansung.ac.kr>
2025-03-30 13:26:01 +0900

    [2주차] 2-(4) 새로운 할인 정책 개발

--- Nayoung/week2/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
+++ Nayoung/week2/core/src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -1,8 +1,9 @@
-package hello.core.order;
+package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
 
+// 정액할인제
 public class FixDiscountPolicy implements DiscountPolicy {
     private int discountFixAmount = 1000;
 
