b816acb559c8ad1d7299f09b1a4a622fdf02cf86
NaYoung <163393527+gimn70009@users.noreply.github.com>
2025-04-03 22:30:31 +0900

    [2주차 과제] 김나영 (#6)
    * [2주차] 1. SpringBoot 프로젝트의 구조
    
    * [2주차] 2-(2) 회원 도메인 설계 및 개발
    
    * [2주차] 2-(3) 주문과 할인 도메인 설계 및 개발
    
    * [2주차] 2-(4) 새로운 할인 정책 개발
    
    * [2주차] 2-(5) 관심사 분리
    
    * [2주차] 2-(6) 스프링으로 전환

---
+++ Nayoung/week2/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,26 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MemberApp {
+    public static void main(String[] args) {
+//       AppConfig appConfig = new AppConfig();
+//       MemberService memberService = appConfig.memberService();
+        // MemberService memberService = new MemberServiceImpl();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = ac.getBean("memberService", MemberService.class);
+
+        Member member = new Member(1L, "lion", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.getMember(1L);
+        System.out.println("findMember : " + findMember.getName());
+        System.out.println("Member : " + member.getName());
+    }
+}
