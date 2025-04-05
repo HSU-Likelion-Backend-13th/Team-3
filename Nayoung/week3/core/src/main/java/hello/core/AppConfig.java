4db19b535a024be324143c846ba6a5559e18cf6d
gimn70009 <risa1110@hansung.ac.kr>
2025-04-05 14:20:33 +0900

    ✨ [3주차]1. 의존관계 자동 주입

---
+++ Nayoung/week3/core/src/main/java/hello/core/AppConfig.java
@@ -0,0 +1,33 @@
+package hello.core;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.*;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public MemberService memberService(){
+        return new MemberServiceImpl(memberRepository());
+        // 요청이 올 때마다 MemberServiceImpl 객체를 생성해서 반환
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
+
+    @Bean
+    public OrderService orderService(){
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    @Bean
+    public DiscountPolicy discountPolicy(){
+        return new RateDiscountPolicy();
+    }
+}
