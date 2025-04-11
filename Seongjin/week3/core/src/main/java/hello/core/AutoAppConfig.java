ab8314d8de72eb1926680d090be6027a6fab38be
oroi2009 <oroi2009@gmail.com>
2025-04-11 14:22:23 +0900

    [과제 제출] 3주차 (#13)
    * feat: 의존관계 자동 주입 실습 코드 추가
    
    * feat: 빈 생명주기 콜백 실습 코드 추가

---
+++ Seongjin/week3/core/src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,16 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.springframework.context.annotation.ComponentScan.*;
+
+@Configuration
+@ComponentScan(
+        // AppConfig를 스캔하지 않도록 하기 위해 @Configuration 어노테이션은 제외(일반적으로는 필요 없는 과정)
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+    // 기존의 AppConfig와는 다르게 @Bean으로 등록한 클래스가 하나도 없다!
+}
---
+++ Seongjin/week3/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,21 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy;
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId); // 사용자 등급을 조회하기 위해
+
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+
+        return new Order(memberId, itemName, itemPrice, discountPrice);
+    }
+}
