d710566b6a3f32a45a99abfa0b310de9311eb2d3
gimn70009 <risa1110@hansung.ac.kr>
2025-05-05 19:05:43 +0900

    ✨ Feat: 양방향 연관관계 매핑 코드 추가
    mappedBy 속성 사용해서 양방향 연관관계 설정

---
+++ Nayoung/week5/jpa-mapping/src/main/java/mapping/Team.java
@@ -0,0 +1,44 @@
+package mapping;
+
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Team {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "TEAM_ID")
+    private Long id;
+
+    @Column(name="NAME")
+    private String name;
+
+    @OneToMany(mappedBy = "team") // 가짜 매핑 (읽기 전용 거울 매핑) -> 주인의 반대편
+    private List<Member> members = new ArrayList<>(); // 이렇게 new ArrayList<>()를 해주는 것은 관례이다.
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
