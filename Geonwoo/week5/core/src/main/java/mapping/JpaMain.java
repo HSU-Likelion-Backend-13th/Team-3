b158c445fcb16f332495e46589dd0555f9463dfe
woodgeon <ad4086@naver.com>
2025-05-07 17:21:39 +0900

    feat: 연관관계 매핑 코드 추가

---
+++ Geonwoo/week5/core/src/main/java/mapping/JpaMain.java
@@ -0,0 +1,52 @@
+package mapping;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            Team team = new Team();
+            team.setName("Team 1");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("Member1");
+            member.setTeam(team);
+            em.persist(member);
+
+            Member member2 = new Member();
+            member2.setUsername("Member2");
+            member2.setTeam(team);
+            em.persist(member2);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for(Member m : members){
+                System.out.println("Member name: " + m.getUsername());
+            }
+
+            tx.commit();
+        }catch (Exception e) {
+            tx.rollback();
+        } finally{
+            em.close();
+        }
+
+        emf.close();
+    }
+}
