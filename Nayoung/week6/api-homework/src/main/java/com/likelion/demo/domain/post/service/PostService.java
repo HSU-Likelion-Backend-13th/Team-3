5330eef141393c4f027942ddea3cc273df0c283b
gimn70009 <risa1110@hansung.ac.kr>
2025-05-12 12:51:31 +0900

    🎉 실습 코드 다운로드

---
+++ Nayoung/week6/api-homework/src/main/java/com/likelion/demo/domain/post/service/PostService.java
@@ -0,0 +1,8 @@
+package com.likelion.demo.domain.post.service;
+
+import com.likelion.demo.domain.post.web.dto.*;
+
+public interface PostService {
+    // 게시글 작성
+    CreatePostRes createOne(CreatePostReq createPostReq);
+}
---
+++ Nayoung/week6/api-homework/src/main/java/com/likelion/demo/domain/post/service/PostServiceImpl.java
@@ -0,0 +1,34 @@
+package com.likelion.demo.domain.post.service;
+
+import com.likelion.demo.domain.post.entity.Post;
+import com.likelion.demo.domain.post.entity.PostState;
+import com.likelion.demo.domain.post.repository.PostRepository;
+import com.likelion.demo.domain.post.web.dto.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@RequiredArgsConstructor
+public class PostServiceImpl implements PostService {
+    private final PostRepository postRepository;
+
+    // 게시글 작성
+    @Override
+    public CreatePostRes createOne(CreatePostReq createPostReq) {
+        // 1. createPostReq가지고 Post Entity 생성
+        Post post = Post.builder()
+                .title(createPostReq.getTitle())
+                .content(createPostReq.getContent())
+                .username((createPostReq.getUsername()))
+                .password(createPostReq.getPassword())
+                .state(PostState.PROGRESS)
+                .build();
+
+        // 2. repository Post 저장 (postRepository 사용)
+        Post savedPost = postRepository.save(post);
+
+        // 3. 반환 CreatePostRes
+        return new CreatePostRes(savedPost.getId());
+    }
+}
